7427fc96f832a76f1867f483fe21bef513c2a1fb
JiaChenx <30770224+JiaChenx@users.noreply.github.com>
2017-08-28 20:23:18 +0800

    BTtree

---
+++ chap2/binary tree.cpp
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <malloc.h> 
+#define MAX 100 
+
+typedef struct node
+{ 
+    int date; 
+    struct node *lchild,*rchild; 
+}bitnode,*bitree; 
+
+bitree createbitree(bitree bt)
+{ 
+    int m; 
+    printf("m="); 
+    scanf("%d",&m); 
+    if(m==-1) 
+    bt=NULL; 
+    else
+    { 
+        bt=(bitnode *)malloc(sizeof(bitnode)); 
+        bt->date=m; 
+        bt->lchild=createbitree(bt->lchild); 
+        bt->rchild=createbitree(bt->rchild); 
+    } 
+    return bt; 
+}
+//set up tree
+void leveltraverse(bitree bt)
+{ 
+    bitree queue[MAX],p=bt; 
+    int rear=0,front=0; 
+    if(p!=NULL)
+    { 
+        queue[++rear]=p; 
+        while(front<rear)
+        { 
+            p=queue[++front]; 
+            printf("%d\t",p->date); 
+            if(p->lchild!=NULL) 
+            queue[++rear]=p->lchild; 
+            if(p->rchild!=NULL) 
+            queue[++rear]=p->rchild; 
+        } 
+    } 
+}
+//preordertraverse
+void preordertraverse(bitree bt)
+{ 
+    if(bt!=NULL)
+    { 
+        printf("%d\t",bt->date); 
+        preordertraverse(bt->lchild); 
+        preordertraverse(bt->rchild); 
+    } 
+}
+//inordertraverse
+void inordertraverse(bitree bt)
+{ 
+    if(bt!=NULL)
+    { 
+        inordertraverse(bt->lchild); 
+        printf("%d\t",bt->date); 
+        inordertraverse(bt->rchild); 
+    } 
+}
+//postordertraverse
+void postordertraverse(bitree bt)
+{ 
+    if(bt!=NULL)
+    {
+        postordertraverse(bt->lchild); 
+        postordertraverse(bt->rchild); 
+        printf("%d\t",bt->date); 
+    } 
+}
+//depth
+int bitreedepth(bitree bt)
+{ 
+    int h,lh,rh; 
+    if(bt==NULL) 
+        h=0; 
+    else
+    { 
+        lh=bitreedepth(bt->lchild); 
+        rh=bitreedepth(bt->rchild); 
+        if(lh>=rh) 
+            h=lh+1; 
+        else 
+            h=rh+1; 
+     
+    } 
+    return h; 
+}
+//leaf of tree
+int bitreeleaf(bitree bt)
+{ 
+    int m=0; 
+    if(!bt) 
+        return 0; 
+    else if(bt->lchild==NULL&&bt->rchild==NULL) 
+        return 1; 
+    m+=bitreeleaf(bt->lchild)+bitreeleaf(bt->rchild); 
+    return m; 
+}
+
+int countbitreenode(bitree bt)
+{ 
+    if(!bt) 
+        return 0; 
+    else 
+        return countbitreenode(bt->lchild)+countbitreenode(bt->rchild)+1; 
+    printf("The postorder traversal of the bitree is:\n\t"); 
+    postordertraverse(bt); 
+    printf("\n\n"); 
+    printf("The number of the bitreenode is %d.\n",countbitreenode(bt)); 
+    printf("The depth of the bitree is %d.\n",bitreedepth(bt)); 
+    printf("The number of the leaves of the bitree is %d.\n\n",bitreeleaf(bt)); 
+}
+
+int main()
+{ 
+    bitree bt;
+    bt=createbitree(bt); 
+}
+
